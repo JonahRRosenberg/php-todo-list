45c09cb9220aac538f1cc4ae7e237ae0da511d2d
Jonah Rosenberg <JonahRRosenberg@gmail.com>
2015-08-31 23:18:17 -0500

    Init todo template

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>My Todo List</title>
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+    <h1>TODO</h1>
+
+    <ul>
+        <li>First item</li>
+        <li>Second item</li>
+    </ul>
+
+    <form>
+        <input type="text" placeholder="What do you have to do?"></input>
+    </form>
+</body>
+
+</html>
